9cc0cc99345bd1d92af1ec7e1836f823062fdfda
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2022-08-15 17:00:05 +0300

    Update zad2p1.c

--- zad2/zad2p1.c
+++ zad2/zad2p1.c
@@ -18,22 +18,33 @@ int onesCount(int mask)
 }
 
 int File_op(char* name){
-    FILE *read_binary = fopen(name, "rb");
-    if (read_binary == NULL)
+    FILE *read = fopen(name, "r");
+    if (read == NULL)
     {
         fprintf(stderr, "%s - not exists!\n", name);
-        return -1;
+        exit(-1);
     }
-    int number[10];
-
-    while (fread(number, sizeof(int), number, read_binary) != EOF)
+    char new_line = getc(read);
+    int line_counter;
+    while (new_line != EOF)
     {
+        if (new_line == '\n')
+            line_counter++;
+        new_line = getc(read);
     }
 
-    fclose(read_binary);
+    rewind(read);
+    int number[line_counter];
 
-    int bits;
-    for (size_t i = 0; i <10; i++)
+    for (size_t i = 0; i < line_counter; i++)
+    {
+        fscanf(read, "%d", &number[i]);
+    }
+
+    fclose(read);
+
+    int bits = 0;
+    for (size_t i = 0; i < line_counter; i++)
     {
         bits += onesCount(number[i]);
     }
@@ -51,6 +62,7 @@ int main(int argc, char** argp){
     }
 
     int totalChild=0;
+    int Cur_child_sum =0;
     
     for (int i = 1; i < n; i++)
     {
@@ -64,29 +76,28 @@ int main(int argc, char** argp){
         if (0 == process_split)
         {
             // child
-            int Cur_child_sum = File_op(argp[i]);
-            printf("%s - %d", argp[i] , Cur_child_sum);
-            totalChild += Cur_child_sum;
-        
-            read(fd[0], &Cur_child_sum, sizeof(Cur_child_sum));
-            write(fd[1], &Cur_child_sum, sizeof(Cur_child_sum));
             close(fd[0]);
-            close(fd[1]);
+            Cur_child_sum = File_op(argp[i]);
+            printf("%s - %d\n", argp[i], Cur_child_sum);
+            read(fd[0], &totalChild, sizeof(totalChild));
+            totalChild += Cur_child_sum;
 
+            write(fd[1], &totalChild, sizeof(totalChild));
+            close(fd[1]);
+            
             return 0;
         }
+         
     }
     // parent
+    close(fd[1]);
     for (int i = 0; i < n; i++) wait(NULL);
     
-    int sum;
     read(fd[0], &totalChild, sizeof(totalChild));
-    write(fd[1], &sum, sizeof(sum));
 
-    close(fd[0]);
-    close(fd[1]);
+    int sum = totalChild;
 
-    printf("Total sum - %d", sum);
+    printf("Total sum - %d\n", sum);
 
     return 0;
 }
